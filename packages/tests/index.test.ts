b97ab08fbc0913e5eca26038ae5bfe5bcd3f5b2a
Joe Pegler <joepegler123@gmail.com>
2024-03-02 01:58:35 +0000

    chore: lint

--- packages/tests/index.test.ts
+++ packages/tests/index.test.ts
@@ -1,6 +1,6 @@
-import { addTwo } from "@biconomy/account";
-import { expect, test } from "vitest";
+import { addTwo } from "@biconomy/account"
+import { expect, test } from "vitest"
 
 test("adds 1 + 2 to equal 3", () => {
-  expect(addTwo(1, 2)).toBe(3);
-});
+  expect(addTwo(1, 2)).toBe(3)
+})
