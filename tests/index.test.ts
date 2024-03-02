2fdc50e8144fb2eecccb7cf720ec6b9195f425a1
Joe Pegler <joepegler123@gmail.com>
2024-03-02 20:44:26 +0000

    chore: vitest tweaks

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,6 +1,6 @@
-import { addTwo } from "~biconomy/core/index.js";
-import { expect, test } from "vitest";
+import { expect, test } from "vitest"
+import { addTwo } from "~biconomy/core/index.js"
 
 test("adds 1 + 2 to equal 3", () => {
-  expect(addTwo(1, 2)).toBe(3);
-});
+  expect(addTwo(1, 2)).toBe(3)
+})
