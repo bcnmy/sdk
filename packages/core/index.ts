935af0e0dea38ae5e1deacadd7df1d84472555a2
Joe Pegler <joepegler123@gmail.com>
2024-03-02 00:12:46 +0000

    chore: add workflows

--- packages/core/index.ts
+++
@@ -1,1 +0,0 @@
-export const addTwo = (a: number, b: number) => a + b
--- packages/test/core.test.ts
+++
@@ -1,6 +0,0 @@
-import { expect, test } from "vitest"
-import { addTwo } from "../core"
-
-test("adds 1 + 2 to equal 3", () => {
-  expect(addTwo(1, 2)).toBe(3)
-})
