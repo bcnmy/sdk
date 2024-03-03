2c0c3989792e5772ba578248d1afc7a74069b6e0
Joe Pegler <joepegler123@gmail.com>
2024-03-03 21:25:32 +0000

    chore: replace bun with process for gh actions

--- tests/account.test.ts
+++ tests/account.test.ts
@@ -1,40 +1,40 @@
-import { beforeAll, describe, test } from "vitest"
+import { beforeAll, describe, test } from "vitest";
 
-import { http, WalletClient, createWalletClient } from "viem"
-import { privateKeyToAccount } from "viem/accounts"
-import { polygonMumbai } from "viem/chains"
-import { createBiconomySmartAccount } from "~@bico/core/index.js"
+import { http, WalletClient, createWalletClient } from "viem";
+import { privateKeyToAccount } from "viem/accounts";
+import { polygonMumbai } from "viem/chains";
+import { createBiconomySmartAccount } from "~@bico/core/index.js";
 
 describe("Account", () => {
-  let smartAccount: Awaited<ReturnType<typeof createBiconomySmartAccount>>
-  let walletClient: WalletClient
+  let smartAccount: Awaited<ReturnType<typeof createBiconomySmartAccount>>;
+  let walletClient: WalletClient;
 
   beforeAll(async () => {
-    console.log(Bun.env.PRIVATE_KEY)
+    console.log(process.env.PRIVATE_KEY);
 
-    const wallet = privateKeyToAccount(`0x${Bun.env.PRIVATE_KEY}`)
+    const wallet = privateKeyToAccount(`0x${process.env.PRIVATE_KEY}`);
 
     walletClient = createWalletClient({
       account: wallet,
-      transport: http(polygonMumbai.rpcUrls.default.http[0])
-    })
+      transport: http(polygonMumbai.rpcUrls.default.http[0]),
+    });
 
     smartAccount = await createBiconomySmartAccount({
       walletClient: walletClient,
-      bundlerUrl: ""
-    })
-  })
+      bundlerUrl: "",
+    });
+  });
 
   test("Create account and get nonce", async () => {
     smartAccount = await createBiconomySmartAccount({
       walletClient: walletClient,
-      bundlerUrl: ""
-    })
+      bundlerUrl: "",
+    });
 
-    const address = await smartAccount.getAccountAddress()
-    console.log("Smart Account Address: ", address)
+    const address = await smartAccount.getAccountAddress();
+    console.log("Smart Account Address: ", address);
 
-    const nonce = await smartAccount.getNonce()
-    console.log("Smart Account Nonce: ", nonce)
-  })
-})
+    const nonce = await smartAccount.getNonce();
+    console.log("Smart Account Nonce: ", nonce);
+  });
+});
