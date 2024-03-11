53052346ce1c559a4ba6417e9ed0cf4d3199287b
Joe Pegler <joepegler123@gmail.com>
2024-03-11 13:10:23 +0000

    chore: add treeshakeable bundler exports

---
+++ src/bundler/index.ts
@@ -0,0 +1,4 @@
+export type { BundlerRpcSchema, BundlerActions } from "./actions/index.js"
+export { bundlerActions } from "./actions/index.js"
+export { extractChainIdFromBundlerUrl } from "./utils/helpers.js"
+export { createBundlerClient } from "./createBundlerClient.js"
