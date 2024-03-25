7a195fb257a6350025613b4b8d5846df96144dcf
Joe Pegler <joepegler123@gmail.com>
2024-03-25 15:32:24 +0000

    chore: remove unnecessary comment

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 export * from "./accounts"
 export * from "./errors"
-export * from "./bundler" // Remove the wildcard export
+export * from "./bundler"
 export * from "./paymaster"
 export * from "./client"
 export * from "./modules"
