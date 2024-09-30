066d26a2d72d390cb365fd5919d59448418c47e1
livingrockrises <90545960+livingrockrises@users.noreply.github.com>
2024-09-30 19:17:55 +0400

    refactor

--- src/sdk/modules/validators/smartSessionValidator/decorators/useEnabledSession.ts
+++ src/sdk/modules/validators/smartSessionValidator/decorators/useEnabledSession.ts
@@ -7,8 +7,8 @@ import {
 import { getAction, parseAccount } from "viem/utils"
 import { AccountNotFoundError } from "../../../../account/utils/AccountNotFound"
 import type { Execution } from "../../../utils/Types"
-import { NexusAccount } from "../../../../account"
-import { ToSmartSessionValidatorModuleReturnType } from "../tosmartSessionValidatorModule"
+import { type NexusAccount } from "../../../../account"
+import { type ToSmartSessionValidatorModuleReturnType } from "../tosmartSessionValidatorModule"
 // Review: Execution type could either be used from our sdk or from module-sdk
 
 // If the session is enabled for multiple actions, it is possible to send a batch transaction. hence it accepts an array of executions.
