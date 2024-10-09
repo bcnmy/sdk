3de0a5bc9201d7c791b14406699da43f2c56fd22
VGabriel45 <vgabrielmarian21@gmai.com>
2024-10-09 13:58:18 +0300

    refactor: lint

--- src/sdk/clients/createNexusSessionClient.ts
+++ src/sdk/clients/createNexusSessionClient.ts
@@ -63,31 +63,31 @@ export type NexusSessionClientConfig<
     client?: client | Client | undefined
     /** Paymaster configuration. */
     paymaster?:
-    | true
-    | {
-      /** Retrieves paymaster-related User Operation properties to be used for sending the User Operation. */
-      getPaymasterData?: PaymasterActions["getPaymasterData"] | undefined
-      /** Retrieves paymaster-related User Operation properties to be used for gas estimation. */
-      getPaymasterStubData?:
-      | PaymasterActions["getPaymasterStubData"]
+      | true
+      | {
+          /** Retrieves paymaster-related User Operation properties to be used for sending the User Operation. */
+          getPaymasterData?: PaymasterActions["getPaymasterData"] | undefined
+          /** Retrieves paymaster-related User Operation properties to be used for gas estimation. */
+          getPaymasterStubData?:
+            | PaymasterActions["getPaymasterStubData"]
+            | undefined
+        }
       | undefined
-    }
-    | undefined
     /** Paymaster context to pass to `getPaymasterData` and `getPaymasterStubData` calls. */
     paymasterContext?: unknown
     /** User Operation configuration. */
     userOperation?:
-    | {
-      /** Prepares fee properties for the User Operation request. */
-      estimateFeesPerGas?:
-      | ((parameters: {
-        account: account | SmartAccount
-        bundlerClient: Client
-        userOperation: UserOperationRequest
-      }) => Promise<EstimateFeesPerGasReturnType<"eip1559">>)
+      | {
+          /** Prepares fee properties for the User Operation request. */
+          estimateFeesPerGas?:
+            | ((parameters: {
+                account: account | SmartAccount
+                bundlerClient: Client
+                userOperation: UserOperationRequest
+              }) => Promise<EstimateFeesPerGasReturnType<"eip1559">>)
+            | undefined
+        }
       | undefined
-    }
-    | undefined
     /** Owner of the session. */
     signer: UnknownSigner
     /** Index of the account. */
@@ -133,7 +133,7 @@ export async function createNexusSessionClient(
     signer,
     accountAddress,
     bundlerUrl,
-    permissionId,
+    permissionId
   } = parameters
 
   if (!chain) throw new Error("Missing chain")
