a6c526623f339dceecf744b827319767906ba603
VGabriel45 <vgabrielmarian21@gmai.com>
2024-11-12 15:57:46 +0200

    refactor: linting

--- src/sdk/clients/decorators/erc7579/moduleActivator.ts
+++ src/sdk/clients/decorators/erc7579/moduleActivator.ts
@@ -5,10 +5,10 @@ import type { ModularSmartAccount } from "../../../modules/utils/Types"
 export type ModuleActions = Record<string, never>
 
 export function moduleActivator(module: Module) {
-    return <TModularSmartAccount extends ModularSmartAccount | undefined>(
-        client: Client<Transport, Chain | undefined, TModularSmartAccount>
-    ): ModuleActions => {
-        client?.account?.setModule(module)
-        return {} as ModuleActions
-    }
+  return <TModularSmartAccount extends ModularSmartAccount | undefined>(
+    client: Client<Transport, Chain | undefined, TModularSmartAccount>
+  ): ModuleActions => {
+    client?.account?.setModule(module)
+    return {} as ModuleActions
+  }
 }
