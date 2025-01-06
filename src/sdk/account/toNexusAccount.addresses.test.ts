f1fb538f897a639c3a34c399d769ea5a93e66ab0
Joe Pegler <joepegler123@gmail.com>
2025-01-06 09:38:59 +0000

    chore: addresses check

---
+++ src/sdk/account/toNexusAccount.addresses.test.ts
@@ -0,0 +1,111 @@
+import {
+  http,
+  type Address,
+  type Chain,
+  type LocalAccount,
+  type PublicClient,
+  type WalletClient,
+  createWalletClient
+} from "viem"
+import { afterAll, beforeAll, describe, expect, test } from "vitest"
+import { toNetwork } from "../../test/testSetup"
+import {
+  fundAndDeployClients,
+  getTestAccount,
+  killNetwork,
+  toTestClient
+} from "../../test/testUtils"
+import type { MasterClient, NetworkConfig } from "../../test/testUtils"
+import type { NexusAccount } from "./toNexusAccount"
+import { getCounterFactualAddress } from "./utils"
+import { createSmartAccountClient } from "../clients/createSmartAccountClient"
+
+describe("nexus.account.addresses", async () => {
+  let network: NetworkConfig
+  let chain: Chain
+  let bundlerUrl: string
+
+  // Test utils
+  let testClient: MasterClient
+  let eoaAccount: LocalAccount
+  let userTwo: LocalAccount
+  let nexusAccountAddress: Address
+  let nexusClient: NexusClient
+  let nexusAccount: NexusAccount
+  let walletClient: WalletClient
+
+  beforeAll(async () => {
+    network = await toNetwork()
+
+    chain = network.chain
+    bundlerUrl = network.bundlerUrl
+    eoaAccount = getTestAccount(0)
+    userTwo = getTestAccount(1)
+    testClient = toTestClient(chain, getTestAccount(5))
+
+    walletClient = createWalletClient({
+      account: eoaAccount,
+      chain,
+      transport: http()
+    })
+
+    nexusClient = await createSmartAccountClient({
+      signer: eoaAccount,
+      chain,
+      transport: http(),
+      bundlerTransport: http(bundlerUrl)
+    })
+
+    nexusAccount = nexusClient.account
+  })
+  afterAll(async () => {
+    await killNetwork([network?.rpcPort, network?.bundlerPort])
+  })
+
+  test("should check account address", async () => {
+    nexusAccountAddress = await nexusClient.account.getCounterFactualAddress()
+    const counterfactualAddressFromHelper = await getCounterFactualAddress(
+      testClient as unknown as PublicClient,
+      eoaAccount.address,
+      true
+    )
+    const gottenAddress = await nexusClient.account.getAddress()
+    expect(counterfactualAddressFromHelper).toBe(nexusAccountAddress)
+    expect(nexusAccount.address).toBe(nexusAccountAddress)
+    expect(nexusAccount.address).toBe(counterfactualAddressFromHelper)
+    expect(gottenAddress).toBe(nexusAccountAddress)
+  })
+
+  test("should check addresses after fund and deploy", async () => {
+    await fundAndDeployClients(testClient, [nexusClient])
+    const counterfactualAddressFromHelper = await getCounterFactualAddress(
+      testClient as unknown as PublicClient,
+      eoaAccount.address,
+      true
+    )
+    const gottenAddress = await nexusClient.account.getAddress()
+    expect(counterfactualAddressFromHelper).toBe(nexusAccountAddress)
+    expect(nexusAccount.address).toBe(nexusAccountAddress)
+    expect(nexusAccount.address).toBe(counterfactualAddressFromHelper)
+    expect(gottenAddress).toBe(nexusAccountAddress)
+  })
+
+  test("should override account address", async () => {
+    const someoneElsesNexusAddress =
+      "0xf0479e036343bC66dc49dd374aFAF98402D0Ae5f"
+    const newNexusClient = await createSmartAccountClient({
+      chain,
+      transport: http(),
+      bundlerTransport: http(bundlerUrl),
+      accountAddress: someoneElsesNexusAddress,
+      signer: eoaAccount
+    })
+    const accountAddress = await newNexusClient.account.getAddress()
+    const someoneElseCounterfactualAddress =
+      await newNexusClient.account.getCounterFactualAddress()
+    expect(newNexusClient.account.address).toBe(
+      someoneElseCounterfactualAddress
+    )
+    expect(accountAddress).toBe(someoneElsesNexusAddress)
+  })
+})
